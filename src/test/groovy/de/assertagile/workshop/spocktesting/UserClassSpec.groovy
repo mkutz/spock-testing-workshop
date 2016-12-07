bb558d08a03c3b69066bd674338335dbf68cffe4
Michael Kutz <michael.kutz@rewe-digital.com>
2016-12-07 15:10:03 +0100

    Made code meet expectations in README.md.

--- src/test/groovy/de/assertagile/workshop/spocktesting/UserClassSpec.groovy
+++ src/test/groovy/de/assertagile/workshop/spocktesting/UserClassSpec.groovy
@@ -1,5 +1,6 @@
 package de.assertagile.workshop.spocktesting
 
+import spock.lang.PendingFeature
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -10,12 +11,27 @@ class UserClassSpec extends Specification {
     @Subject
     User user
 
+    def "isOfAge should return false if the user's bithday is less than 18 years ago"() {
+        given:
+        user = new User("jdoe", "John", "Doe", LocalDate.now().minusYears(17))
+
+        when:
+        boolean isOfAge = user.isOfAge()
+
+        then:
+        !isOfAge
+    }
+
+    @PendingFeature
     def "isOfAge should return true if the user's birthday is 18 years ago"() {
         given:
         user = new User("jdoe", "John", "Doe", LocalDate.now().minusYears(18))
 
-        expect:
-        boolean result = user.isOfAge()
+        when:
+        boolean isOfAge = user.isOfAge()
+
+        then:
+        isOfAge
     }
 
     def "isOfAge should return true uf the user's birthday is more than 18 years ago"() {
